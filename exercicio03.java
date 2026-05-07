22acd352686939e7a3b46b0988b5cfc9ac35dac2
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-06 23:46:23 -0300

    É para estar pronto

--- exercicio03.java
+++ exercicio03.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class exe03 {
+public class exercicio03 {
 
     public static void main(String[] args) {
         /*     3. Escrever um programa que
@@ -32,29 +32,33 @@ matriz C */
         List<Double> A = new ArrayList<>();
         List<Double> B = new ArrayList<>();
         List<Double> C = new ArrayList<>();
+        double resultado;
 
         for (int i = 0; i < 20; i++) {
             listaA = cpu.nextDouble(100) + 1;
             listaA = Math.round(listaA * 10.0) / 10.0;
-            System.out.println(listaA);
             A.add(listaA);
 
         }
+        System.out.println(A);
+        System.out.println("\n");
 
         for (int i = 0; i < 20; i++) {
             listaB = cpu.nextDouble(100) + 1;
             listaB = Math.round(listaB * 10.0) / 10.0;
-            System.out.println(listaB);
-            A.add(listaB);
-            
-        for (int i = 0; i < 20; i++) {
-            listaB = cpu.nextDouble(100) + 1;
-            listaB = Math.round(listaB * 10.0) / 10.0;
-            System.out.println(listaB);
-            A.add(listaB);
-
+            B.add(listaB);
         }
+        System.out.println(B);
+        System.out.println("\n");
+        for (int i = 0; i < 20; i++) {
+            C.add(A.get(i) - B.get(i));
+            C.set(i,(Math.round(C.get(i) * 10.0) / 10.0));
+            if(C.get(i)< 0){
+                C.set(i, 0.0);
+            }
+        }
+        System.out.println("\n");
+        System.out.println(C);
         
-
     }
 }
