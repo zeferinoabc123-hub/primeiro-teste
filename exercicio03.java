948e928cb8b22103bdbbf9a84666c63fd6b50307
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-06 21:40:29 -0300

    primeira versao do ex03

---
+++ exercicio03.java
@@ -0,0 +1,60 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class exe03 {
+
+    public static void main(String[] args) {
+        /*     3. Escrever um programa que
+leia duas matrizes
+(denominadas A e B) do tipo
+vetor com 20 elementos
+reais. Construir uma matriz C,
+sendo cada elemento da
+matriz C a subtração de um
+elemento correspondente da
+matriz A com um elemento
+correspondente da matriz B,
+ou seja, a operação de
+processamento deve estar
+baseada na operação C[I] ←
+A[I] – B[I]. Ao final,
+apresentar os elementos da
+matriz C */
+
+        Scanner entrada = new Scanner(System.in);
+        Random cpu = new Random();
+        double listaA;
+        double listaB;
+
+        List<Double> A = new ArrayList<>();
+        List<Double> B = new ArrayList<>();
+        List<Double> C = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            listaA = cpu.nextDouble(100) + 1;
+            listaA = Math.round(listaA * 10.0) / 10.0;
+            System.out.println(listaA);
+            A.add(listaA);
+
+        }
+
+        for (int i = 0; i < 20; i++) {
+            listaB = cpu.nextDouble(100) + 1;
+            listaB = Math.round(listaB * 10.0) / 10.0;
+            System.out.println(listaB);
+            A.add(listaB);
+            
+        for (int i = 0; i < 20; i++) {
+            listaB = cpu.nextDouble(100) + 1;
+            listaB = Math.round(listaB * 10.0) / 10.0;
+            System.out.println(listaB);
+            A.add(listaB);
+
+        }
+        
+
+    }
+}
